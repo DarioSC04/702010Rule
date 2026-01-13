e79b857a59dde88b467d1e9687b6911a76a5a7ab
marlonhorn <149269855+marlonhorn@users.noreply.github.com>
2026-01-13 11:25:41 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '702010Rule',
+  webDir: 'www'
+};
+
+export default config;
